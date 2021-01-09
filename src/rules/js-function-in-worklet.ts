6132c04f252d4872916fcedad17732bc43d31a07
William Candillon <william.candillon@shopify.com>
2021-01-09 15:00:53 +0100

    :four:

--- src/rules/js-function-in-worklet.ts
+++ src/rules/js-function-in-worklet.ts
@@ -1,12 +1,9 @@
-import { ESLintUtils, 
-  AST_NODE_TYPES,
-  TSESTree, } from '@typescript-eslint/experimental-utils';
-
+import { ESLintUtils, TSESTree, } from '@typescript-eslint/experimental-utils';
+import {isFunctionDeclaration, isBlock, isExpressionStatement} from "typescript";
 const createRule = ESLintUtils.RuleCreator(
     name =>
       `https://github.com/wcandillon/eslint-plugin-reanimated/blob/master/rules/${name}.md`,
 );
-
 export type Options = [
 
 ];
@@ -31,15 +28,25 @@ export default createRule<Options, MessageIds>({
   },
   defaultOptions: [],
   create: (context) => {
-    const sourceCode = context.getSourceCode();
+    //const sourceCode = context.getSourceCode();
     const parserServices = ESLintUtils.getParserServices(context);
     const checker = parserServices.program.getTypeChecker();
-    const compilerOptions = parserServices.program.getCompilerOptions();
+    //const compilerOptions = parserServices.program.getCompilerOptions();
     return {
       CallExpression: (node) => {
         const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node);
-        const t = checker.getTypeAtLocation(tsNode);
-        console.log({ t });
+        const signature = checker.getResolvedSignature(tsNode);
+        const decl = signature?.declaration;
+        if (decl !== undefined && isFunctionDeclaration(decl)) {
+          if (decl.body && isBlock(decl.body)) {
+            const statement = decl.body.statements[0];
+            if (statement && isExpressionStatement(statement)) {
+              const esNode = parserServices.tsNodeToESTreeNodeMap.get<TSESTree.ExpressionStatement>(statement);
+              const isWorklet = esNode.directive === "worklet";
+              console.log({isWorklet});
+            }
+          }
+        }
       }
     }
   }
